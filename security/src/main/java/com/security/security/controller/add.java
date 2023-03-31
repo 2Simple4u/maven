8d54e72189c7bc4c98280eaabad101da758ca0d7
Singh <deepak.s.singh@capgemini.com>
2023-03-31 16:22:41 +0530

    first commit

---
+++ security/src/main/java/com/security/security/controller/add.java
@@ -0,0 +1,13 @@
+package com.security.security.controller;
+
+public class add {
+
+    public void print(){
+        System.out.print("this test is run");
+    }
+
+    public static void main(String[] args){
+        add a = new add();
+        a.print();
+    }
+}
